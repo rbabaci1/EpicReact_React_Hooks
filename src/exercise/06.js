1cb56bbb1197e8243ebd3f9e2c98a5afa8f56a65
rabah babaci <4rabah@gmail.com>
2021-02-21 08:14:28 -0800

    use a status state to make the code more clear

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -15,28 +15,42 @@ import {
 
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+  const [status, setStatus] = React.useState('idle')
 
   React.useEffect(() => {
     if (!pokemonName) return
 
-    setPokemon(null)
+    setStatus('pending')
 
-    fetchPokemon(pokemonName)
-      .then(pokemonData => {
+    fetchPokemon(pokemonName).then(
+      pokemonData => {
         setPokemon(pokemonData)
-      })
-      .catch(error => {
-        console.error(error)
-      })
+        setStatus('resolved')
+      },
+      error => {
+        setError(error)
+        setStatus('rejected')
+      },
+    )
   }, [pokemonName])
 
-  return !pokemonName ? (
-    'Submit a pokemon'
-  ) : !pokemon ? (
-    <PokemonInfoFallback name={pokemonName} />
-  ) : (
-    <PokemonDataView pokemon={pokemon} />
-  )
+  if (status === 'idle') {
+    return 'Submit a pokemon'
+  } else if (status === 'pending') {
+    return <PokemonInfoFallback name={pokemonName} />
+  } else if (status === 'rejected') {
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  } else if (status === 'resolved') {
+    return <PokemonDataView pokemon={pokemon} />
+  }
+
+  throw new Error('This should be impossible')
 }
 
 function App() {
